e17993553dc1234196e3344d289e46172bab6cc6
Ashok Patel <ashokpatel881095@gmail.com>
2025-03-31 22:33:57 +0530

    Deploying App

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API = "http://localhost:5000/api/auth/user"
-export const JOB_API = "http://localhost:5000/api/auth/job"
-export const APPLICATION_API = 'http://localhost:5000/api/auth/application'
-export const COMPANY_API = 'http://localhost:5000/api/auth/company'
+export const USER_API = "https://deploy-job-portal-3.onrender.com/api/auth/user"
+export const JOB_API = "https://deploy-job-portal-3.onrender.com/api/auth/job"
+export const APPLICATION_API = 'https://deploy-job-portal-3.onrender.com/api/auth/application'
+export const COMPANY_API = 'https://deploy-job-portal-3.onrender.com/api/auth/company'
